cb424c8985f5bc13b964d01c206a00d26607c66a
peterOutOfLowerGumtree <jwhiteley0@icloud.com>
2017-08-01 17:30:12 +0100

    end of day commit 010817

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,4 +10,28 @@ class Application extends Controller {
     Ok(views.html.index("Home"))
   }
 
+  def bookingConfirmation = Action {
+    Ok(views.html.bookingConfirmation())
+  }
+
+  def write = Action {
+    writeToFile()
+    Ok.sendFile(new java.io.File("hello.txt"), inline = false)
+  }
+
+  def writeToFile(): Unit = {
+    val pw = new PrintWriter(new File("hello.txt"))
+    pw.write("Hello, world")
+    pw.close()
+  }
+
+  def delete = Action {
+    deleteFile()
+    Redirect(routes.Application.bookingConfirmation())
+  }
+
+  def deleteFile(): Unit = {
+    new File("hello.txt").delete()
+  }
+
 }
